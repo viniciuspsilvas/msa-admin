d6b47c7c5193fe141e8a11307290cc84a31f6318
Vinnie <viniciuspsilvas@gmail.com>
2019-08-21 15:49:01 +1000

    Updated host

--- src/util/apiClient.js
+++ src/util/apiClient.js
@@ -1,16 +1,7 @@
 var axios = require('axios');
 
-let url = null
-if (process.env.NODE_ENV === 'development') {
-    url = 'http://phpstack-209922-714883.cloudwaysapps.com'
-}
-
-if (process.env.NODE_ENV === 'production') {
-    url = process.env.REACT_APP_SERVER_URL;
-}
-
 var axiosInstance = axios.create({
-    baseURL: url,
+    baseURL: process.env.REACT_APP_SERVER_URL,
     timeout: 3000,
     /* other custom settings */
 });
