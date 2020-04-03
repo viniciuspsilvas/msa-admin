d1c31d0f27da474de429a50c5315d30b54b617fe
Vinnie <viniciuspsilvas@gmail.com>
2020-04-03 21:18:50 +1000

    Fixing crazy bug

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from "react-redux";
 
-const PrivateRoute = ({isAuthenticated, component: Component, ...rest, }) => {
+const PrivateRoute = ({isAuthenticated, component: Component, ...rest }) => {
 
     return (
 
